9e288a9b46c8ffdcc9e332fc6fc33d55c28febc2
Greg Hild <grhild@gmail.com>
2013-09-10 16:04:04 -0500

    Add class Person methods

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,22 @@
+
 class Person
+
+	@@run = 5
+	@@scream = 10
+	@@drink_coffee =100
+
+	def initialize(name, caffeine_level)
+		@name = name
+		@caffeine_level = caffeine_level
+	end
+	
+	def run(miles)
+		puts "Person runs #{miles} miles."
+	end
+
+	def scream(decibals)
+		puts "Person screams #{decibals}dB load"
+	end
 end
 
 class PowerRanger
